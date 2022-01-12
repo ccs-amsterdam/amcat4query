ef4af39756764ccb9e6535e62cd5376abe66263e
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-01-12 13:25:10 +0100

    Fold amcat4lib into amcat4query and add stub aggregate component

---
+++ src/lib/Aggregate/Aggregate.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+/**
+ *
+ * @param {class}  amcat  An Amcat connection class, as obtained with amcat4auth
+ * @param {string} index The name of an index
+ * @param {object} query An object with query components (q, params, filter)
+ */
+export default function Aggregate({ amcat, index, query }) {
+  return <pre>Aggregate</pre>;
+}
