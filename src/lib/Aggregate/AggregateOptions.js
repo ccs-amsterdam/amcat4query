20ab0fd689f35724db54b55151a61040cc46a9e7
Wouter van Atteveldt <wouter@vanatteveldt.com>
2022-01-16 20:50:04 +0100

    Move axis picker to separate component

--- src/lib/Aggregate/AggregateOptions.js
+++ src/lib/Aggregate/AggregateOptions.js
@@ -1,53 +1,26 @@
 import { useState } from "react";
-import { Button, Dropdown } from "semantic-ui-react";
+import { Button, Form } from "semantic-ui-react";
 import useFields from "../components/useFields";
-
-const icon_date = "https://img.icons8.com/material-outlined/24/000000/calendar--v1.png";
-const icon_keyword = "https://img.icons8.com/material-outlined/24/000000/activity-feed.png";
+import AxisPicker from "./AxisPicker";
 
 export default function AggregateOptions({ amcat, index, setOptions }) {
   const fields = useFields(amcat, index);
-  const [axis1, setAxis1] = useState();
-  const [axis2, setAxis2] = useState();
+  const [axis1, setAxis1] = useState(null);
+  const [axis2, setAxis2] = useState(null);
 
   function doSetOptions() {
-    const axes = [axis1, axis2].filter((x) => x !== undefined && x !== "");
+    const axes = [axis1, axis2].filter((axis) => axis !== null);
     setOptions(axes);
   }
 
-  var axisOptions = []; //{ key: "None", value: null, text: "" }];
-  for (let f in fields) {
-    if (fields[f] === "keyword" || fields[f] === "date")
-      axisOptions.push({
-        key: f,
-        text: f,
-        value: f,
-        image: { avatar: true, src: fields[f] === "date" ? icon_date : icon_keyword },
-      });
-  }
   return (
-    <div>
-      <Dropdown
-        placeholder="Primary axis"
-        clearable
-        fluid
-        selection
-        options={axisOptions}
-        value={axis1}
-        onChange={(e, { value }) => setAxis1(value)}
-      />
-      <Dropdown
-        placeholder="Secondary axis"
-        clearable
-        fluid
-        selection
-        options={axisOptions}
-        value={axis2}
-        onChange={(e, { value }) => setAxis2(value)}
-      />
+    <Form>
+      <AxisPicker fields={fields} setAxis={setAxis1} label="Primary Aggregation Axis" />
+      <AxisPicker fields={fields} setAxis={setAxis2} label="Secondary Aggregation Axis" />
+
       <Button primary onClick={doSetOptions}>
         Submit
       </Button>
-    </div>
+    </Form>
   );
 }
---
+++ src/lib/Aggregate/AxisPicker.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import { Form } from "semantic-ui-react";
+
+const icon_date = "https://img.icons8.com/material-outlined/24/000000/calendar--v1.png";
+const icon_keyword = "https://img.icons8.com/material-outlined/24/000000/activity-feed.png";
+
+const date_intervals = [
+  { key: "day", text: "day", value: "day" },
+  { key: "week", text: "week", value: "week" },
+  { key: "month", text: "month", value: "month" },
+  { key: "quarter", text: "quarter", value: "quarter" },
+  { key: "year", text: "year", value: "year" },
+];
+
+function getAxisSpec(field, type, interval) {
+  if (field === undefined || field === "") return null;
+  if (type === "date") return { field, interval };
+  return { field };
+}
+
+export default function AggregateOptions({ fields, setAxis, label }) {
+  const [field, setField] = useState();
+  const [interval, setInterval] = useState();
+
+  let axisOptions = [];
+  for (let f in fields) {
+    if (fields[f] === "keyword" || fields[f] === "date")
+      axisOptions.push({
+        key: f,
+        text: f,
+        value: f,
+        image: { avatar: true, src: fields[f] === "date" ? icon_date : icon_keyword },
+      });
+  }
+
+  useEffect(() => {
+    setAxis(getAxisSpec(field, fields[field], interval));
+  }, [interval, field, fields, setAxis]);
+
+  return (
+    <Form.Group widths="equal">
+      <Form.Dropdown
+        placeholder="Select field for aggregation axis"
+        clearable
+        fluid
+        selection
+        options={axisOptions}
+        label={label ? label : "Aggregation axis"}
+        value={field}
+        onChange={(_e, { value }) => setField(value)}
+      />
+      {fields[field] === "date" ? (
+        <Form.Dropdown
+          placeholder="Select interval for date aggregation"
+          fluid
+          selection
+          options={date_intervals}
+          value={interval}
+          onChange={(_e, { value }) => setInterval(value)}
+        />
+      ) : null}
+    </Form.Group>
+  );
+}
